c110a407566786376e2607876e678feff1520692
Marcos Hernandez <marcos.hernandez181190@hotmail.com>
2023-03-31 23:19:33 -0600

    Se realiza la configuración de los parametros de la base en el archivo .properties

---
+++ src/main/java/com/telcel/repositoriooym/RepositoriooymBackendApplication.java
@@ -0,0 +1,13 @@
+package com.telcel.repositoriooym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepositoriooymBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepositoriooymBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/telcel/repositoriooym/RepositoriooymBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.telcel.repositoriooym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepositoriooymBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
