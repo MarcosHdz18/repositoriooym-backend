e955ae1ae5a70a4fa567662d81e432d00b6b9331
Marcos Hernandez <marcos.hernandez181190@hotmail.com>
2023-04-01 12:34:45 -0600

    Se realiza la creación de las interfaces IAreaResponsableProyecto e IResponsableProyecto

---
+++ src/main/java/com/telcel/repositoriooym/repository/IAreaResponsableProyecto.java
@@ -0,0 +1,11 @@
+package com.telcel.repositoriooym.repository;
+
+import com.telcel.repositoriooym.entity.AreaResponsableProyecto;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * @author marcos.hernandez
+ */
+
+public interface IAreaResponsableProyecto extends CrudRepository<AreaResponsableProyecto, Long> {
+}
---
+++ src/main/java/com/telcel/repositoriooym/repository/IResponsableProyecto.java
@@ -0,0 +1,11 @@
+package com.telcel.repositoriooym.repository;
+
+import com.telcel.repositoriooym.entity.ResponsableProyecto;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * @author marcos.hernandez
+ */
+
+public interface IResponsableProyecto extends CrudRepository<ResponsableProyecto, Long> {
+}
